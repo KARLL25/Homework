bffd62bace5fb8a14983f52b7bb779915ea13f49
KARLL25 <51706588+KARLL25@users.noreply.github.com>
2022-03-10 22:56:22 +0800

    Update gameSprite.py

--- 1.2/pygame Sprite/gameSprite.py
+++ 1.2/pygame Sprite/gameSprite.py
@@ -1,8 +1,7 @@
 import pygame
 import random
 from tkinter import *
-
-from pygame.locals import(
+from pygame.locals import (
     K_UP,
     K_DOWN,
     K_LEFT,
@@ -11,90 +10,99 @@ from pygame.locals import(
     KEYDOWN,
     QUIT,
     RLEACCEL
-    )
+)
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
 
-SCREEN_WIDTH=800
-SCREEN_HEIGHT=800
+playerSprite = pygame.sprite.Sprite()
+enemySprite = pygame.sprite.Sprite()
+enemySpeed = 5
+bg = pygame.image.load("nebo.jpg")
 
-
-PlayerSprite=pygame.sprite,Sprite()
-enemySprite=pygame.sprite.Sprite()
-enemySpeed=5
-background_image=pygame.image.load("nebo.jpg")
 
 def playerInit():
     global playerSprite
-    playerSprite.image=pygame.image.load("samolet.png").convert()
-    playerSprite.image.set_colorkey((255,255,255), RLEACCEL)
-    playerSprite.rect=playerSprite.image.get_rect()
+    # playerSprite.image = pygame.Surface((75, 25))
+    playerSprite.image = pygame.image.load("samolet.jpg").convert()
+    # playerSprite.image.fill((255,255,255))
+    playerSprite.image.set_colorkey((255, 255, 255), RLEACCEL)
+    playerSprite.rect = playerSprite.image.get_rect()
+
 
 def playerUpdate(pressed_keys):
     global playerSprite
     if pressed_keys[K_UP]:
-        playerSprite.rect.move_ip(0,-5)
+        playerSprite.rect.move_ip(0, -5)
     if pressed_keys[K_DOWN]:
-        playerSprite.rect.move_ip(0,5)
-    if pressed_key[K_LEFT]:
-        playerSprite.rect.move_ip(-5,0)
+        playerSprite.rect.move_ip(0, 5)
+    if pressed_keys[K_LEFT]:
+        playerSprite.rect.move_ip(-5, 0)
     if pressed_keys[K_RIGHT]:
-        playerSprite.rect.move_ip(5,0)
+        playerSprite.rect.move_ip(5, 0)
 
-    if playerSprite.rect.left<0:
-        playerSprite.rect.left=0
-    if playerSprite.rect.right>SCREEN_WIDTH:
-        playerSprite.rect.right=SCREEN_WIDTH
-    if playerSprite.rect.top<=0:
-        playerSprite.rect.top=0
-    if playerSprite.rect.bottom>=SCREEN_HEIGHT:
-        playerSprite.rect.bottom=SCREEN_HEIGHT
+    if playerSprite.rect.left < 0:
+        playerSprite.rect.left = 0
+    if playerSprite.rect.right > SCREEN_WIDTH:
+        playerSprite.rect.right = SCREEN_WIDTH
+    if playerSprite.rect.top <= 0:
+        playerSprite.rect.top = 0
+    if playerSprite.rect.bottom >= SCREEN_HEIGHT:
+        playerSprite.rect.bottom = SCREEN_HEIGHT
+
 
 def enemyInit():
     global enemySpeed
     global enemySprite
-    enemySprite.image=pygame.image.load("raketa.png").convert()
-    enemySprite.image.set_colorkey((255,255,255),RLEACCEL)
-    enemySprite.rect= enemySprite.image.get_rect(
+    # enemySprite.image = pygame.Surface((20, 10))
+    enemySprite.image = pygame.image.load("raketa.png").convert()
+    # enemySprite.image.fill((255,255,255))
+    enemySprite.image.set_colorkey((255, 255, 255), RLEACCEL)
+    enemySprite.rect = enemySprite.image.get_rect(
         center=(
-            random.randint(SCREEN_WIDTH+20,SCREEN_WIDTH+100),
-            random.randint(0,SCREEN_HEIGHT),
-            )
+            random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+            random.randint(0, SCREEN_HEIGHT),
         )
-    enemySpeed=random.randint(5,10)
+    )
+    enemySpeed = random.randint(5, 10)
+
 
 def enemyUpdate():
     global enemySprite
-    enemySprite.rect.move_ip(-enemySpeed,0)
-    if enemySprite.rect.right<0:
-        enemySprite.rect= enemySprite.image.get_rect(
-        center=(
-            random.randint(SCREEN_WIDTH+20,SCREEN_WIDTH+100),
-            random.randint(0,SCREEN_HEIGHT),
+    enemySprite.rect.move_ip(-enemySpeed, 0)
+    if enemySprite.rect.right < 0:
+        enemySprite.rect = enemySprite.image.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
             )
         )
-pygame.init()
-screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 playerInit()
 enemyInit()
-clock=pygame.time.Clock()
-running=True
+clock = pygame.time.Clock()
+running = True
+
 while running:
     for event in pygame.event.get():
-        for event.type==KEYDOWN:
-            if event.key==K_ESCAPE:
-                running=False
-            elif event.type==QUIT:
-                running=False
-    pressed_keys=pygame.key.get_pressed()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+    pressed_keys = pygame.key.get_pressed()
     playerUpdate(pressed_keys)
     enemyUpdate()
+
 
     if pygame.sprite.collide_rect(playerSprite, enemySprite):
         playerInit()
         enemyInit()
-        
         try:
             import Tkinter as tk
         except:
@@ -103,17 +111,21 @@ while running:
         root = tk.Tk()
         root.geometry("200x200")
 
-        button = tk.Button(text = "Начать заново", command = root.destroy)
+
+        button = tk.Button(text="Попробовать снова", command=root.destroy)
         button.pack()
         root.geometry('200x200-805-505')
         root.mainloop()
-        
-        window = tk.Tk()
-        window.mainloop()
-        
-    #screen.fill()
-    screen.blit(background_image,(0,0))
 
+        window = tk.Tk()
+        window.title("PyGame Sprite")
+
+        root.geometry("200x200")
+        window.mainloop()
+    screen.fill((0, 0, 0))
+    screen.blit(bg, (0, 0))
+
+    # screen.blit(playerSprite.image, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
     screen.blit(playerSprite.image, playerSprite.rect)
     screen.blit(enemySprite.image, enemySprite.rect)
 
@@ -121,3 +133,6 @@ while running:
     clock.tick(60)
 
 pygame.quit()
+
+
+
