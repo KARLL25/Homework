2d8bf68427c0bd377a45159bc3d175db79fde106
KARLL25 <51706588+KARLL25@users.noreply.github.com>
2022-04-28 23:27:02 +0800

    Update snake.py

--- 1.2/snakeGame/snake.py
+++ 1.2/snakeGame/snake.py
@@ -10,18 +10,26 @@ dirs={'W':True,'S':True,'A':True,'D':True,}
 length= 1
 snake= [(x,y)]
 dx,dy= 0,0
+score=0
 fps=8
+
 
 pygame.init()
 sc= pygame.display.set_mode([RES,RES])
 clock=pygame.time.Clock()
+font_score= pygame.font.SysFont('Arial', 26, bold=True)
+font_end= pygame.font.SysFont('Arial', 26, bold=True)
 
 while True:
     sc.fill(pygame.Color('black'))
 
     #цвет змейки и яблок
-    [(pygame.draw.rect(sc, pygame.Color('green'),(i, j, SIZE, SIZE))) for i,j in snake]
+    [(pygame.draw.rect(sc, pygame.Color('green'),(i, j, SIZE - 2, SIZE - 2))) for i,j in snake]
     pygame.draw.rect(sc, pygame.Color('red'),(*apple, SIZE, SIZE))
+
+    #Счет
+    render_score= font_score.render(f'Счёт: {score}', 1, pygame.Color('orange'))
+    sc.blit(render_score,(5, 5))
 
     #Передвижение змейки
     x+=dx*SIZE
@@ -31,14 +39,19 @@ while True:
     
     #Поедание яблок
     if snake[-1] == apple:
-        apple= randrange(0, RES, SIZE),randrange(0, RES, SIZE)
+        apple = randrange(0, RES, SIZE),randrange(0, RES, SIZE)
         length+=1
+        score+=1
         
     #Игра окончена
-    if x<0 or x >RES-SIZE or y<0 or y>RES-SIZE:
-        break
-    if len(snake) != len(set(snake)):
-        break
+    if x < 0 or x > RES - SIZE or y<0 or y>RES - SIZE or len(snake) != len(set(snake)):
+        while True:
+            render_end= font_end.render('Вы проиграли',1, pygame.Color('orange'))
+            sc.blit(render_end,(RES // 2 - 200, RES // 3))
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit()
         
     pygame.display.flip()
     clock.tick(fps)
@@ -51,14 +64,14 @@ while True:
     key=pygame.key.get_pressed()
     if key[pygame.K_w] and dirs['W']:
         dx,dy= 0,-1
-        dirs={'W':True,'S':False,'A':True,'D':True,}
+        dirs={'W': True,'S': False,'A': True,'D': True,}
     if key[pygame.K_s] and dirs['S']:
         dx,dy= 0,1
-        dirs={'W':False,'S':True,'A':True,'D':True,}
+        dirs={'W': False,'S': True,'A': True,'D': True,}
     if key[pygame.K_a] and dirs['A']:
         dx,dy= -1,0
-        dirs={'W':True,'S':True,'A':True,'D':False,}
+        dirs={'W': True,'S': True,'A': True,'D': False,}
     if key[pygame.K_d] and dirs['D']:
         dx,dy= 1,0
-        dirs={'W':True,'S':True,'A':False,'D':True,}
+        dirs={'W': True,'S': True,'A': False,'D': True,}
     
