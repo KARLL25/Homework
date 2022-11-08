92a307e4688c56e26e2449e3971a3d1702e20c3f
KARLL25 <51706588+KARLL25@users.noreply.github.com>
2022-11-08 13:06:01 +0800

    Create knowledge.py

---
+++ 2.1/11.08_starie_znania/knowledge.py
@@ -0,0 +1,40 @@
+def obmen(): #zadacha 1
+    c,a,b=[input('a:'),input('b:'),input('c:')]
+    print(a,b,c)
+obmen()
+
+def check(): #zadanie 2.1
+    a=(input('Введите первое число:'))
+    b=(input('Введите второе число'))
+    while True:
+        try:
+            a=float(a)
+            b=float(b)
+        except ValueError:
+            print('Надо вводить числа')
+            break
+
+        else:
+            print(a + b)
+check()
+
+def vvod(): #zadanie 2.2
+    n = int(input('Введите количество чисел:'))
+    a=[]
+    for i in range(n):
+        a.append(int(input(f'Введите переменную N{i+1}: ')))
+    print(a)
+vvod()
+
+def diapazon():
+    x=int(input('Введите любое число от 0 до 100'))
+    try:
+        if x in range(101):
+            print(x*5)
+    except IndexError:
+        print('Вы выбрали число не из диапазона от 0 до 100')
+    else:
+        print(x*5)
+diapazon()
+
+
