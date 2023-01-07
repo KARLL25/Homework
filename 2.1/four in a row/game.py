31f2a8f9e37066e2844a0b0bd5fb0810c7c9aa85
KARLL25 <51706588+KARLL25@users.noreply.github.com>
2023-01-07 21:06:43 +0800

    Update game.py

--- 2.1/four in a row/game.py
+++ 2.1/four in a row/game.py
@@ -1,148 +1,135 @@
-import numpy as np
-import pygame
-import sys
-import math
-
-BLUE = (0,0,255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-YELLOW = (255,255,0)
-
-ROW_COUNT = 6
-COLUMN_COUNT = 6
-
-def create_board():
-	board = np.zeros((ROW_COUNT,COLUMN_COUNT))
-	return board
-
-def drop_piece(board, row, col, piece):
-	board[row][col] = piece
-
-def is_valid_location(board, col):
-	return board[ROW_COUNT-1][col] == 0
-
-def get_next_open_row(board, col):
-	for r in range(ROW_COUNT):
-		if board[r][col] == 0:
-			return r
-
-def print_board(board):
-	print(np.flip(board, 0))
-
-def winning_move(board, piece):
-	# проверка по горизонтали
-	for c in range(COLUMN_COUNT-3):
-		for r in range(ROW_COUNT):
-			if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
-				return True
-
-	# проверка по вертикали
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT-3):
-			if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
-				return True
-
-	# проверка "положительной" диагонали
-	for c in range(COLUMN_COUNT-3):
-		for r in range(ROW_COUNT-3):
-			if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
-				return True
-
-	# проверка "отрицательной" диагонали
-	for c in range(COLUMN_COUNT-3):
-		for r in range(3, ROW_COUNT):
-			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
-				return True
-
-def draw_board(board):
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT):
-			pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
-			pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
-	
-	for c in range(COLUMN_COUNT):
-		for r in range(ROW_COUNT):		
-			if board[r][c] == 1:
-				pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-			elif board[r][c] == 2: 
-				pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
-	pygame.display.update()
-
-
-board = create_board()
-print_board(board)
-game_over = False
-turn = 0
-
+import pygame, sys, pygame_menu
+from tkinter import *
+from tkinter import messagebox
+ 
 pygame.init()
+ 
+ 
+class Chip(): # Класс для рисования фишек на поле
+    
+    def __init__(self, x, y):
+        self.color = WHITE
+        self.rect = (x, y, 65, 65)
+ 
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+ 
+class Button(): # Создаем кнопки для меню
 
-SQUARESIZE = 100
-
-width = COLUMN_COUNT * SQUARESIZE
-height = (ROW_COUNT+1) * SQUARESIZE
-
-size = (width, height)
-
-RADIUS = int(SQUARESIZE/2 - 5)
-
+    def __init__(self, width, height, x, y):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        
+ 
+    def draw(self, screen, text, rect_color, border_color):
+        border = 2
+        pygame.draw.rect(screen, rect_color, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(screen, border_color, (self.x + border, self.y + border, self.width - border*2, self.height - border*2))
+        screen.blit(text, (self.x + self.width / 6, self.y + self.height / 7))
+ 
+def vertical_win(column):
+        cubes = 0
+        for i in range(len(column) - 1):
+                        if column[i].color == RED and column[i+1].color == RED or column[i].color == YELLOW and column[i+1].color == YELLOW:
+                            cubes += 1
+                        if cubes == 3:
+                            winner = column[i].color
+                            Tk().wm_withdraw()
+                            messagebox.showinfo('Result', f"{column[i].color} is a winner")
+                            return False
+     
+def horisontal_win(chips):
+    cubes = 0
+    for i in range(len(chips) - 1):
+        for j in range(len(chips[i])):
+            if chips[i][j].color == RED and chips[i + 1][j].color == RED or chips[i][j].color == YELLOW and chips[i+1][j].color == YELLOW:
+                cubes +=1
+            if cubes == 6:
+                winner = chips[i + 1][j].color
+                Tk().wm_withdraw()
+                messagebox.showinfo('Result', f"{chips[i + 1][j].color} is a winner")
+                return False
+ 
+                
+            
+ 
+size = (650, 425)
 screen = pygame.display.set_mode(size)
-draw_board(board)
-pygame.display.update()
-
-myfont = pygame.font.SysFont("monospace", 75)
-
-while not game_over:
-
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-
-		if event.type == pygame.MOUSEMOTION:
-			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-			posx = event.pos[0]
-			if turn == 0:
-				pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
-			else: 
-				pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
-		pygame.display.update()
-
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
-			
-			# ввод первым игроком
-			if turn == 0:
-				posx = event.pos[0]
-				col = int(math.floor(posx/SQUARESIZE))
-
-				if is_valid_location(board, col):
-					row = get_next_open_row(board, col)
-					drop_piece(board, row, col, 1)
-
-					if winning_move(board, 1):
-						label = myfont.render("Player 1 wins!!", 1, RED)
-						screen.blit(label, (40,10))
-						game_over = True
-
-
-			# ввод вторым игроком
-			else:				
-				posx = event.pos[0]
-				col = int(math.floor(posx/SQUARESIZE))
-
-				if is_valid_location(board, col):
-					row = get_next_open_row(board, col)
-					drop_piece(board, row, col, 2)
-
-					if winning_move(board, 2):
-						label = myfont.render("Player 2 wins!!", 1, YELLOW)
-						screen.blit(label, (40,10))
-						game_over = True
-
-			print_board(board)
-			draw_board(board)
-
-			turn += 1
-			turn = turn % 2
-
-			if game_over:
-				pygame.time.wait(3000)
+    
+YELLOW = 'yellow'
+WHITE = 'white'
+BLACK = 'black'
+RED = 'red'
+ 
+my_font = pygame.font.SysFont('monospace', 42)
+my_font_2 = pygame.font.SysFont('monospace', 24)
+ 
+pygame.display.set_caption("4 в ряд")
+clock = pygame.time.Clock()
+ 
+rows = [5, 75, 145, 215, 285, 355]
+columns = [5, 75, 145, 215, 285, 355]
+ 
+chips = [[Chip(x, y) for y in columns] for x in rows]
+ 
+def run_game():
+    turn = 0
+    run = True
+    rows_of_objects = [[],[],[],[],[],[]]
+    while run:
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                (posx,posy) = pygame.mouse.get_pos()
+                rows.append(posx)
+                rows.sort()
+                column_num = rows.index(posx) - 1
+                rows.remove(posx)
+ 
+                if menu_button.x < posx < menu_button.x + menu_button.width and menu_button.y < posy < menu_button.y + menu_button.height:
+                    run = False
+ 
+                if column_num >= 0 and posx < rows[column_num] + 70:
+                    column = chips[column_num]
+                    row = column[column_num]
+                    
+                    for chip in column[::-1]:
+                        if chip.color == WHITE:
+                            if turn == 0:
+                                chip.color = RED
+                            else:
+                                chip.color = YELLOW
+                            turn = 1 - turn
+                            break
+                    vertical_win(column)
+                    horisontal_win(chips)
+                
+                   
+                    
+        screen.fill(BLACK)
+ 
+        for i in chips:
+            for chip in i:
+                chip.draw(screen)
+        
+        menu_button = Button(150, 65, 495, 355)
+        quit_text = my_font.render('Menu', True, WHITE)
+        menu_button.draw(screen, quit_text, WHITE, BLACK)
+        
+        pygame.display.flip()
+ 
+ 
+def finish_game():
+    pygame.quit()
+    sys.exit()
+ 
+ 
+menu = pygame_menu.Menu('4 в ряд', 650, 425, theme = pygame_menu.themes.THEME_DARK)
+menu.add.button('Play', run_game)
+menu.add.button('Quit', finish_game)
+menu.mainloop(screen)
